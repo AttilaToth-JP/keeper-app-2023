129681adbb7f4ca53bc47b1a88bd2a74cb2c99ba
Attila <atomicdevkitten@gmail.com>
2023-02-15 18:17:40 +0000

    second challenge - amend notes.js file name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../Note"
+import notes from "../notes"
 
 const App = function () {
   return (
