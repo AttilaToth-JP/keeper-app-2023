0c542909c1dca92456553fa486c534df027bc550
Attila <atomicdevkitten@gmail.com>
2023-02-15 18:10:57 +0000

    second challenge - render all notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../Note"
 
 const App = function () {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((item) => {
+        return (
+          <Note 
+            key={item.id} 
+            title={item.title} 
+            content={item.content}
+          />);
+      })}
       <Footer />
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Note = function () {
+const Note = function (props) {
   return (
     <div className="note">
-      <h1>This is the title of note</h1>
-      <p>This is the para of note</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 };
